a5caa5d1048af325fbb508006a2dfb17cd8a4ba8
Ilyes <ilyes.smaoui94@gmail.com>
2025-11-21 05:18:51 +0100

    Started adding some code

---
+++ src/common/common_types.hpp
@@ -0,0 +1,7 @@
+// Assumptions/rules for the price :
+// price is always > 0
+// ?
+struct Price {
+  // Now, the question is, how do we store the price ?
+};
+using price_t = Price;
---
+++ src/pricing/option_pricing.hpp
@@ -0,0 +1,12 @@
+#include "../common/common_types.hpp"
+
+struct BlackScholesInputs {
+  price_t curr_price{};
+};
+
+price_t getOptionPrice(BlackScholesInputs &bs_inputs) {
+  price_t final_price{};
+  // TO-DO : implement and add parameters
+  //
+  return final_price;
+}
